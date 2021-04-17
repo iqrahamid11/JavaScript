2ef6ef6abc45dde60219ae36cf2e8d85a203b8f6
Iqra Hamid <hamidiqra55@gmail.com>
2021-04-17 09:33:45 -0700

    Work Accomplish

---
+++ app.js
@@ -0,0 +1,169 @@
+    // chap # 1 (alert)
+
+
+
+
+
+
+// 1. Write a script to greet your website visitor using JS alert BhxBrowser.apply
+// 2. Write a script to display following message on your web page:
+
+// var a= ("Error! Please enter a valid password.");
+// alert (a);
+
+
+// 3. write a script to display following message on your web page: (Hint : Use line break) 
+
+// var b= ("Welcome to JS Land... \nHappy Coding!") ;
+// alert  (b);
+
+// 4.Write a script to display following messages in sequence:
+
+// var c= ("Welcome to JS Land");
+// alert (c);
+
+// 5.Generate the following message through browser's developer console:
+
+// var d= ("Hello... I can run JS through my web brower's console");
+// alert (d);
+
+
+
+
+
+// chap # 2 (variables for strings)
+
+
+
+
+
+
+// 1. Declare a variable called username.
+// 2. Declare a variable called myname & assign to it a string that represent your Full Name.
+// 3. Write script to
+//      a) Declare a JS variable, titled message.
+//      b) Assign " Hello World" to variable message
+//      c) Display the message in alert box.
+//      4. Write a script to save student's bio data in JS variable and show the data in alert boxes.
+
+// alert ("Hello World")
+// var name= ("jhone doe");
+// alert (name);
+
+// var age= ("15 years old");
+// alert (age);
+
+// var bio= ("Certified Mobile Applicatiom Development");
+// alert (bio);
+
+// Q NO5
+
+// var q= ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+// alert (q)
+
+// Q NO6
+
+// var email= ("My email address is example@example.com");
+// alert (email);
+
+// Q NO7
+
+// var book= ("A smarter way to learn JavaScript");
+// var message= ("I am trying to learn from the Book A smarter \nway to learn JavaScript");
+// alert (message);
+ 
+// Q NO8 (index.html)
+
+
+// Q no9
+// alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+// chap #3 VARIABLE FOR NUMBERS    
+
+// Q NO1
+
+// var a= ("I am ");
+// var age=  15 ;
+// var ge= (" years old");
+// var years= (a+age+ge);
+// alert (years)
+
+// Q NO2
+
+// var visit= ("You have visited this site ");
+// var time= 14;
+// var times= (" times");
+// var visitTime= (visit+time+times);
+// alert (visitTime);
+
+
+// Q NO3 (index.html)
+
+
+// chap 9-11(USER INPUT & CONDITIONAL STATEMENT)
+//  QNO1
+
+// var city= prompt ("Enter your city name");
+// if(city ==karachi) {
+//     alert("Welcome to city of lights")
+// }
+
+// QNO2
+
+// var gender= +prompt ("Gender")
+
+// if (gender==male){
+
+//    alert(" Good Morning Sir.")
+// }
+
+// else if(gender==female){
+
+// alert ("God Morning Ma'am. ")
+// }
+
+
+
+// QNO3
+
+// var a= +prompt ("road traffic signals");
+
+// if (a== red){
+
+//     alert("Must Stop")
+// }
+// else if (a==yellow){
+
+//     alert("Ready to move")
+// }
+
+// else if (a==green){
+
+//     alert ("Move now")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
